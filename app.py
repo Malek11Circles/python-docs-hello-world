cab768ca8687da0f37a1a1d796749b45b78d32a2
Malek11Circles <87654895+Malek11Circles@users.noreply.github.com>
2021-07-19 16:54:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World...This is Malek From Circles Information Technology!"
+    return "Hello, World...This is Malek From Circles Information Technology!
+كل عام وانتم بخير"
