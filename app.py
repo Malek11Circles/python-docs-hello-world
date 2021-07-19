121af0c5e509ef1afa33869759009e4335efbf15
Malek11Circles <87654895+Malek11Circles@users.noreply.github.com>
2021-07-19 18:18:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello, World...This is Malek From Circles Information Technology!..كل عام وانتم بخير"
-
+<img src="{{url_for('static', filename='67fab5ad7a00ffc72b01a9fb98984068-799x565.png')}}" align="middle" />
